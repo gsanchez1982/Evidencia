97eb55022f812674f06a3649caf249ab1bd95274
Guillermo <gsemx@outlook.com>
2021-06-23 15:29:29 -0500

    Update menuPrincipal.java

--- src/menuPrincipal.java
+++ src/menuPrincipal.java
@@ -9,7 +9,8 @@ public class menuPrincipal {
         System.out.println("[1] -- Doctores");
         System.out.println("[2] -- Pacientes");
         System.out.println("[3] -- Crear una nueva cita");
-        System.out.println("[4] -- Salir del Programa");
+        System.out.println("[4] -- Cambiar de usuario");
+        System.out.println("[5] -- Salir del Programa");
         System.out.println("");
         System.out.println("Escriba el número de su opción a continuación:");
 
@@ -19,11 +20,16 @@ public class menuPrincipal {
         switch(x) {
             case "1":
                 System.out.println("Doctores");
+                System.exit(0);
             case "2":
                 System.out.println("Pacientes");
+                System.exit(0);
             case "3":
                 System.out.println("Citas");
+                System.exit(0);
             case "4":
+                logIn.inicio();
+            case "5":
                 System.out.println("El programa ha terminado exitosamente. ¡Hasta luego!");
                 System.exit(0);
             default:
